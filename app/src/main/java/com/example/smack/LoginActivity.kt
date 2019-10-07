37212e4c9975924f7654224ea1988368f4b90d77
simon <simonhanwilson@icloud.com>
2019-10-07 08:48:52 +0100

    added Create Activity and intent from LoginActivity screen to createLogin screen

--- app/src/main/java/com/example/smack/LoginActivity.kt
+++ app/src/main/java/com/example/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -19,7 +20,8 @@ class LoginActivity : AppCompatActivity() {
 
     fun loginCreateUserBtnClicked(view: View) {
 
-
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
 
     }
 }
