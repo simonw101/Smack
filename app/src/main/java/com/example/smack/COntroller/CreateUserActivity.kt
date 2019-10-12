6d55362423f8e7680b19ad6ed86e1ca7dc56fd78
simonw101 <simonhanwilson@icloud.com>
2019-10-12 18:58:48 +0100

    implemented user signup

--- app/src/main/java/com/example/smack/COntroller/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/COntroller/CreateUserActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smack.R
+import com.example.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -69,7 +70,15 @@ class CreateUserActivity : AppCompatActivity() {
 
     fun createUserClicked(view: View) {
 
+        AuthService.registerUser(this, "simonhanwilson@icloud.com", "pass123") {complete ->
 
+            if (complete) {
+
+                
+
+            }
+
+        }
 
     }
 }
